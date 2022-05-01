efd690de4b8ca72e5d2ed78072da03b618a9acbe
Felipe <fquiceno1@misena.edu.co>
2022-05-01 14:41:29 -0500

    Efectos con useEffect

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 -> En los componentes de funcion poodemos crear multiples estados, en los componentes de clase No, solo tiene un estado en el contructor y se deben pasar las props.
 
+-> El useEffect en componentes de funccion es equivalente metodos de ciclo de vida en componentes de clases
 -> 
 ------
 
@@ -12,3 +13,6 @@
 
 # Estados Simples
 - se crea variables de estado para el componente de clase y el componente de funcion
+
+# Efectos con useEffect
+- Se crea un estado para mostrar un mensjae de carga en un parrafo simulando un request al backend
--- src/UseState.js
+++ src/UseState.js
@@ -2,7 +2,24 @@ import React from "react";
 
 function UseState({name}){
     const [error,setError] = React.useState(false);
+    const {loading, setLoading} = React.useState(false);
 
+    React.useEffect(()=> {
+        console.log('Empezando el efecto');
+
+        if(loading){
+
+            setTimeout(()=>{
+                console.log('Haciendo la validación');
+    
+                setLoading(false);
+    
+                console.log('Terminando la validación');
+            },3000);
+        }
+
+        console.log('Terminando el efecto');
+    },[loading])
     return(
         <div>
                 <h2>Eliminar {name}</h2>
@@ -11,6 +28,9 @@ function UseState({name}){
                 
                 {error && (
                     <p>Error: El codigo es incorrecto</p>
+                )}
+                {loading && (
+                    <p>Cargando...</p>
                 )}
 
                 <input placeholder="Código de Seguridad"/>
