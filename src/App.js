b537aefd620245d256e72cb859f6343312eba32b
Felipe <fquiceno1@misena.edu.co>
2022-05-01 13:57:08 -0500

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {UseState} from './UseState.js';
+import {ClassState} from './ClassState.js';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+      <ClassState />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UseState.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function UseState(){
+    return(
+        <div>
+                <h2>Eliminar UseState</h2>
+                
+                <p>Por favor, Escribe le codigo de seguridad</p>
+                
+                <input placeholder="Código de Seguridad"/>
+                <button>Comprobar</button>
+
+            </div>
+    );
+
+}
+
+export {UseState};
